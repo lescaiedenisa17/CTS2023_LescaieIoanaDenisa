764cc7a655d6bb83dfe75163cfc458d1e9a25d9b
Student <lescaieioana20@stud.ase.ro>
2023-03-08 11:13:12 +0200

    third commit

--- Desktop/Java2021-main/Java2021-main/Tema1_CTS_SOLID/src/ro/ase/acs/sql/interfaces/Insertor.java
+++ Desktop/Java2021-main/Java2021-main/Tema1_CTS_SOLID/src/ro/ase/acs/sql/interfaces/Insertor.java
@@ -3,6 +3,6 @@ package ro.ase.acs.sql.interfaces;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public interface Insertor{
+public interface Insertor {
     void insertData(Connection connection) throws SQLException;
 }
--- Desktop/Java2021-main/Java2021-main/Tema1_CTS_SOLID/src/ro/ase/acs/sql/main/Main.java
+++ Desktop/Java2021-main/Java2021-main/Tema1_CTS_SOLID/src/ro/ase/acs/sql/main/Main.java
@@ -1,36 +1,38 @@
 package ro.ase.acs.sql.main;
 import ro.ase.acs.sql.classes.DataInserter;
+import ro.ase.acs.sql.classes.DataInserterWithParams;
 import ro.ase.acs.sql.classes.DataReader;
 import ro.ase.acs.sql.classes.TableCreator;
+import ro.ase.acs.sql.interfaces.Creator;
+import ro.ase.acs.sql.interfaces.Insertor;
+import ro.ase.acs.sql.interfaces.Reader;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
-
     public static void main(String[] args) {
         try {
             Class.forName("org.sqlite.JDBC");
             Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
             connection.setAutoCommit(false);
 
-            TableCreator tableCreator = new TableCreator();
+            Creator tableCreator = new TableCreator();
             tableCreator.createTable(connection);
 
-            DataInserter dataInserter = new DataInserter();
+            Insertor dataInserter = new DataInserter();
             dataInserter.insertData(connection);
 
-            DataReader dataReader = new DataReader();
+            Insertor dataInserterWithParams = new DataInserterWithParams();
+            dataInserterWithParams.insertData(connection);
+
+            Reader dataReader = new DataReader();
             dataReader.readData(connection);
 
             connection.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
